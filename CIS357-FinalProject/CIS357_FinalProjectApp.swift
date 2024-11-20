a855f89613509c68bf20510f53635d6da9224412
Kyle Fink <kylefink3@gmail.com>
2024-11-20 13:16:46 -0500

    Initial Commit

---
+++ CIS357-FinalProject/CIS357_FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CIS357_FinalProjectApp.swift
+//  CIS357-FinalProject
+//
+//  Created by Kyle J. Fink on 11/20/24.
+//
+
+import SwiftUI
+
+@main
+struct CIS357_FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
